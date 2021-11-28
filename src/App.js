cc7d1a4a84873519db7f57833b69ed8b99517643
hrj0903 <heerij93@gmail.com>
2021-11-28 21:15:13 +0900

    toDoList react

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
-function Hello() {
-  useEffect(() => {
-    console.log('created :)');
-    //cleanup function(컴포넌트가 destroy될때 사용)
-    //ex.컴포넌트가 없어질때 어떤 분석 결과를 보내고 싶을때,
-    //또는 eventlistener를 지우고 싶을때
-    return () => console.log('destroyed :(');
-  }, []);
-  return <h1>Hello</h1>;
-}
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState('');
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === '') {
+      return;
+    }
+    setToDo('');
+    setToDos((currentArray) => [toDo, ...currentArray]);
+  };
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? 'Hide' : 'Show'}</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          value={toDo}
+          onChange={onChange}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
