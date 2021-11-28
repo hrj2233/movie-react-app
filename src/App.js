41ae5c3f9107c9d17ef1bf3d2c3750487611e2fc
hrj0903 <heerij93@gmail.com>
2021-11-28 20:34:29 +0900

    cleanup function

--- src/App.js
+++ src/App.js
@@ -1,44 +1,22 @@
 import { useState, useEffect } from 'react';
 
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState('');
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => {
-    setKeyword(event.target.value);
-  };
-
-  // 한번만 렌더링 되고 실행안됨.
-  // 아무것도 보고 있지 않기 때문에.
+function Hello() {
   useEffect(() => {
-    console.log('i run only once.');
+    console.log('created :)');
+    //cleanup function(컴포넌트가 destroy될때 사용)
+    //ex.컴포넌트가 없어질때 어떤 분석 결과를 보내고 싶을때,
+    //또는 eventlistener를 지우고 싶을때
+    return () => console.log('destroyed :(');
   }, []);
-
-  // keyword가 변할때만 다시 렌더링
-  useEffect(() => {
-    console.log("I run when 'keyword' changes.");
-  }, [keyword]);
-
-  // counter가 변할때만 다시 렌더링
-  useEffect(() => {
-    console.log("I run when 'counter' changes.");
-  }, [counter]);
-
-  // keyword 또는 counter가 변할때 다시 렌더링
-  useEffect(() => {
-    console.log('I run when keyword & counter change.');
-  }, [keyword, counter]);
-
+  return <h1>Hello</h1>;
+}
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here...."
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? 'Hide' : 'Show'}</button>
     </div>
   );
 }
