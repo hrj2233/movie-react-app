212e689d75d82f01a5caf786447401943485e431
hrj0903 <heerij93@gmail.com>
2021-11-28 19:36:01 +0900

    useeffect example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState('');
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => {
+    setKeyword(event.target.value);
+  };
+
+  // 한번만 렌더링 되고 실행안됨.
+  // 아무것도 보고 있지 않기 때문에.
+  useEffect(() => {
+    console.log('i run only once.');
+  }, []);
+
+  // keyword가 변할때만 다시 렌더링
+  useEffect(() => {
+    console.log("I run when 'keyword' changes.");
+  }, [keyword]);
+
+  // counter가 변할때만 다시 렌더링
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
+
+  // keyword 또는 counter가 변할때 다시 렌더링
+  useEffect(() => {
+    console.log('I run when keyword & counter change.');
+  }, [keyword, counter]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here...."
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me</button>
     </div>
   );
 }
